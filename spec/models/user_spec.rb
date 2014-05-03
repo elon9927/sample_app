6a0332210e59241dd5f6e9284024f25ca69b327f
Elon <65289927@qq.com>
2014-05-03 17:18:37 +0800

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -54,7 +54,7 @@ describe User do
       end
     end
   end
-
+=begin
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
@@ -63,6 +63,16 @@ describe User do
     end
 
     it { should_not be_valid }
+  end
+=end
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
   end
 
   describe "when password is not present" do
